c1d2bdcd509ecef9d3e56fee016b45399ad7c999
Jeremy (Xiao) Pian <xpian@yelp.com>
2012-12-19 10:46:54 -0800

    initial commit

---
+++ open_table.py
@@ -0,0 +1,30 @@
+import time
+import urllib2
+from batch import MDBatch
+
+class OpenTableChecker(MDBatch.MDBatch):
+	notify_emails = ['xpian@yelp.com']
+	date = '31'
+	def run(self):
+		while True:
+			url =\
+			'http://www.opentable.com/opentables.aspx?t=rest&r=40507&m=74&p=2&d=12/%s/2012%%207:00:00%%20PM&scpref=114'
+			try:
+				page = urllib2.urlopen(url % self.date)
+				dom = page.read()
+			except:
+				continue
+			num_avail_times = dom.count('<span class="t">')
+			print 'number of available times is %s' % num_avail_times
+			if 'No tables are available' in dom:
+				print 'Not Found'
+				self.notify_email('NOT!')
+			else:
+				print 'Found!'
+				self.notify_email('Found! Hurry!')
+				print 'Email sent!'
+			time.sleep(60)
+
+
+if __name__ == '__main__':
+	OpenTableChecker().run()
